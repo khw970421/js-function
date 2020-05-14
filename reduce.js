3fe91e6804b2bc08c024353496b85a63966cbee3
김형욱 <59253551+khw970421@users.noreply.github.com>
2020-05-15 01:55:46 +0900

    Update reduce.js

--- reduce.js
+++ reduce.js
@@ -75,3 +75,6 @@ console.log(result1); // [2, 6, 10]
 
 var result2 = data.filter(x => x % 2 != 0).map(x => x * 2);
 console.log(result2); // [2, 6, 10]
+
+출처 : https://developer.mozilla.org/ko/docs/Web/JavaScript/Reference/Global_Objects/Array/Reduce
+https://medium.com/@hongkevin/js-3-%EC%9E%90%EB%B0%94%EC%8A%A4%ED%81%AC%EB%A6%BD%ED%8A%B8-%EB%B0%B0%EC%97%B4-%EB%A9%94%EC%84%9C%EB%93%9C-reduce-100-%ED%99%9C%EC%9A%A9%EB%B2%95-feat-egghead-io-97c679857ece
