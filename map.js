d4b087ed83cf14ce90e8129b3407cae3cce7a8f5
khw970421 <khw970421@kakao.com>
2020-05-21 23:46:03 +0900

    map 함수

---
+++ map.js
@@ -0,0 +1,9 @@
+//map() 메서드는 배열 내의 모든 요소 각각에 대하여 주어진 함수를 호출한 결과를 모아 새로운 배열을 반환합니다.
+
+const array1 = [1, 4, 9, 16];
+
+// pass a function to map
+const map1 = array1.map(x => x * 2);
+
+console.log(map1);
+// expected output: Array [2, 8, 18, 32]
